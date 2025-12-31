13d6bfacc9ee3c53075bbab0068e340d778f504d
Marcos "Marcão" Aurelio <macosaures@gmail.com>
2025-12-31 00:37:21 -0300

    Initialize PHP project with Composer, PHPUnit, and PHP-CS-Fixer configuration. Added .gitignore, LICENSE, and PHPStan setup. Created directory structure for source and tests.

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2025 PXP
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/pxp-sh/pdf
+ *
+ */
+
+use Ergebnis\License;
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$license = License\Type\MIT::text(
+    __DIR__ . '/LICENSE',
+    License\Range::since(
+        License\Year::fromString('2025'),
+        new DateTimeZone('UTC')
+    ),
+    License\Holder::fromString('PXP'),
+    License\Url::fromString('https://github.com/pxp-sh/pdf')
+);
+
+$license->save();
+
+$finder = Finder::create()
+    ->in(__DIR__)
+    ->append(glob(__DIR__ . '/*.php'))
+    ->append(glob(__DIR__ . '/.*.php'))
+    ->contains('*.php');
+
+return (new Config())
+    ->setFinder($finder)
+    ->setRules([
+        'header_comment' => [
+            'comment_type' => 'PHPDoc',
+            'header' => $license->header(),
+            'location' => 'after_declare_strict',
+            'separate' => 'both',
+        ],
+    ]);
